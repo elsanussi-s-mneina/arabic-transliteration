e800ca3f191660bf70fc7513703c2275be404037
Elsanussi Mneina <elsanussimneina@Elsanussis-MacBook-Pro.local>
2019-07-09 02:40:47 -0400

    Transliteration of alif madda.

--- src/Buckwalter.hs
+++ src/Buckwalter.hs
@@ -2,3 +2,4 @@ module Buckwalter where
 
 romanization :: Char -> Char
 romanization 'ء' = '\''
+romanization 'آ' = '|'
--- test/BuckwalterTests.hs
+++ test/BuckwalterTests.hs
@@ -10,3 +10,5 @@ buckwalterSpecs =
     describe "romanization" $ do
       it "should return an apostrophe for ARABIC LETTER HAMZA" $ do
         romanization 'ء' `shouldBe` '\''
+      it "should return a pipe for ARABIC LETTER ALEF WITH MADDA ABOVE" $ do
+        romanization 'آ' `shouldBe` '|'
