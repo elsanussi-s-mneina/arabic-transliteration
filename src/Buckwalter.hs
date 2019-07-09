a2ed1b0c906ae8f71bedfca05dcce05dbf16195b
Elsanussi Mneina <elsanussimneina@Elsanussis-MacBook-Pro.local>
2019-07-09 03:01:24 -0400

    Added romanization of more letters.

--- src/Buckwalter.hs
+++ src/Buckwalter.hs
@@ -3,3 +3,10 @@ module Buckwalter where
 romanization :: Char -> Char
 romanization 'ء' = '\''
 romanization 'آ' = '|'
+romanization 'أ' = '>'
+romanization 'ؤ' = '&'
+romanization 'إ' = '<'
+romanization 'ئ' = '}'
+romanization 'ا' = 'A'
+romanization 'ب' = 'b'
+romanization 'ة' = 'p'
--- test/BuckwalterTests.hs
+++ test/BuckwalterTests.hs
@@ -12,3 +12,24 @@ buckwalterSpecs =
         romanization 'ء' `shouldBe` '\''
       it "should return a pipe for ARABIC LETTER ALEF WITH MADDA ABOVE" $ do
         romanization 'آ' `shouldBe` '|'
+      it ("should return a greater than sign for ARABIC LETTER ALEF " ++
+          "WITH HAMZA ABOVE") $ do
+        romanization 'أ' `shouldBe` '>'
+      it ("should return an ampersand for ARABIC LETTER WAW " ++
+          "WITH HAMZA ABOVE") $ do
+        romanization 'ؤ' `shouldBe` '&'
+      it ("should return a less than sign for " ++
+          "ARABIC LETTER ALEF WITH HAMZA BELOW") $ do
+        romanization 'إ' `shouldBe` '<'
+      it ("should return a right brace for " ++
+          "ARABIC LETTER YEH WITH HAMZA ABOVE") $ do
+        romanization 'ئ' `shouldBe` '}'
+      it ("should return a capital A for " ++
+          "ARABIC LETTER ALEF") $ do
+        romanization 'ا' `shouldBe` 'A'
+      it ("should return a lowercase b for " ++
+          "ARABIC LETTER BEH") $ do
+        romanization 'ب' `shouldBe` 'b'
+      it ("should return a lowercase p for " ++
+          "ARABIC LETTER TEH MARBUTA") $ do
+        romanization 'ة' `shouldBe` 'p'
