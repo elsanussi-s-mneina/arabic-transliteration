08a50513ff4e6e891a211a53f5393387f73d8297
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-10-29 16:07:15 -0500

    style: whitespace

--- src/ISO233_2.hs
+++ src/ISO233_2.hs
@@ -1,174 +1,147 @@
 module ISO233_2 where
 
 romanization_iso232 char = case char of
-  '\x0621'
-     -> 'ʾ'
+  '\x0621' -> 'ʾ'
 -- ء
 -- ARABIC LETTER HAMZA
 -- Unicode: U+0621
-  '\x0628'
-     -> 'b'
+
+  '\x0628' -> 'b'
 -- ب
 -- ARABIC LETTER BEH
 -- Unicode: U+0628
 
-  '\x062A'
-     -> 't'
+  '\x062A' -> 't'
 -- ت
 -- ARABIC LETTER TEH
 -- Unicode: U+062A
 
-  '\x062B'
-     -> 'ṯ'
+  '\x062B' -> 'ṯ'
 -- ث
 -- ARABIC LETTER THEH
 -- Unicode: U+062B
 
-  '\x062C'
-     -> 'ǧ'
+  '\x062C' -> 'ǧ'
 -- ج
 -- ARABIC LETTER JEEM
 -- Unicode: U+062C
 
-  '\x062D'
-     -> 'ḥ'
+  '\x062D' -> 'ḥ'
 -- ح
 -- ARABIC LETTER HAH
 -- Unicode: U+062D
 
-  '\x062E'
-     -> 'ẖ'
+  '\x062E' -> 'ẖ'
 -- خ
 -- ARABIC LETTER KHAH
 -- Unicode: U+062E
 
-  '\x062F'
-     -> 'd'
+  '\x062F' -> 'd'
 -- د
 -- ARABIC LETTER DAL
 -- Unicode: U+062F
 
-  '\x0630'
-     -> 'ḏ'
+  '\x0630' -> 'ḏ'
 -- ذ
 -- ARABIC LETTER THAL
 -- Unicode: U+0630
 
-  '\x0631'
-     -> 'r'
+  '\x0631' -> 'r'
 -- ر
 -- ARABIC LETTER REH
 -- Unicode: U+0631
 
-  '\x0632'
-     -> 'z'
+  '\x0632' -> 'z'
 -- ز
 -- ARABIC LETTER ZAIN
 -- Unicode: U+0632
 
-  '\x0633'
-     -> 's'
+  '\x0633' -> 's'
 -- س
 -- ARABIC LETTER SEEN
 -- Unicode: U+0633
 
-  '\x0634'
-     -> 'š'
+  '\x0634' -> 'š'
 -- ش
 -- ARABIC LETTER SHEEN
 -- Unicode: U+0634
 
-  '\x0635'
-     -> 'ṣ'
+  '\x0635' -> 'ṣ'
 -- ص
 -- ARABIC LETTER SAD
 -- Unicode: U+0635
 
-  '\x0636'
-     -> 'ḍ'
+  '\x0636' -> 'ḍ'
 -- ض
 -- ARABIC LETTER DAD
 -- Unicode: U+0636
 
-  '\x0637'
-     -> 'ṭ'
+  '\x0637' -> 'ṭ'
 -- ط
 -- ARABIC LETTER TAH
 -- Unicode: U+0637
 
-  '\x0638'
-     -> 'ẓ'
+  '\x0638' -> 'ẓ'
 -- ظ
 -- ARABIC LETTER ZAH
 -- Unicode: U+0638
 
-  '\x0639'
-     -> 'ʿ'
+  '\x0639' -> 'ʿ'
 -- ع
 -- ARABIC LETTER AIN
 -- Unicode: U+0639
 
-  '\x063A'
-     -> 'ġ'
+  '\x063A' -> 'ġ'
 -- غ
 -- ARABIC LETTER GHAIN
 -- Unicode: U+063A
 
-  '\x0641'
-     -> 'f'
+  '\x0641' -> 'f'
 -- ف
 -- ARABIC LETTER FEH
 -- Unicode: U+0641
 
-  '\x0642'
-     -> 'q'
+  '\x0642' -> 'q'
 -- ق
 -- ARABIC LETTER QAF
 -- Unicode: U+0642
 
-  '\x0643'
-     -> 'k'
+  '\x0643' -> 'k'
 -- ك
 -- ARABIC LETTER KAF
 -- Unicode: U+0643
 
-  '\x0644'
-     -> 'l'
+  '\x0644' -> 'l'
 -- ل
 -- ARABIC LETTER LAM
 -- Unicode: U+0644
 
-  '\x0645'
-     -> 'm'
+  '\x0645' -> 'm'
 -- م
 -- ARABIC LETTER MEEM
 -- Unicode: U+0645
 
-  '\x0646'
-     -> 'n'
+  '\x0646' -> 'n'
 -- ن
 -- ARABIC LETTER NOON
 -- Unicode: U+0646
 
-  '\x0647'
-     -> 'h'
+  '\x0647' -> 'h'
 -- ه
 -- ARABIC LETTER HEH
 -- Unicode: U+0647
 
-  '\x0629'
-     -> 'ẗ'
+  '\x0629' -> 'ẗ'
 -- ة
 -- ARABIC LETTER TEH MARBUTA
 -- Unicode: U+0629
 
-  '\x0648'
-     -> 'w'
+  '\x0648' -> 'w'
 -- و
 -- ARABIC LETTER WAW
 -- Unicode: U+0648
-  '\x064A'
-     -> 'y'
+
+  '\x064A' -> 'y'
 -- ي
 -- ARABIC LETTER YEH
 -- Unicode: U+064A
